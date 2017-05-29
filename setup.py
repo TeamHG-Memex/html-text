f56c0bac048d9278e5842758255ac86d4b8f09e0
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-05-29 16:05:43 +0300

    0.2.1 with a packaging fix

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='html_text',
-    version='0.2.0',
+    version='0.2.1',
     description="Extract text from HTML",
     long_description=readme + '\n\n' + history,
     author="Konstantin Lopukhin",
