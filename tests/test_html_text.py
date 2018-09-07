0f2fb2b840d71538cf874027a8026167fd96d2e6
Ludovica Gonella <ludovica.gonella@gmail.com>
2018-09-07 12:45:17 -0700

    fix indentation

--- tests/test_html_text.py
+++ tests/test_html_text.py
@@ -2,16 +2,21 @@
 import pytest
 import lxml
 
-from html_text import (extract_text, html_to_text, parse_html, parse_html,
+from html_text import (extract_text, html_to_text, parse_html,
                        cleaned_selector, selector_to_text)
 
 
-@pytest.fixture(params=[{'guess_punct_space': True},
-                        {'guess_punct_space': False},
-                        {'guess_punct_space': True, 'guess_page_layout': True},
-                        {'guess_punct_space': False, 'guess_page_layout': True}
-                        ])
-
+@pytest.fixture(params=[{
+    'guess_punct_space': True
+}, {
+    'guess_punct_space': False
+}, {
+    'guess_punct_space': True,
+    'guess_page_layout': True
+}, {
+    'guess_punct_space': False,
+    'guess_page_layout': True
+}])
 def all_options(request):
     return request.param
 
@@ -51,8 +56,9 @@ def test_punct_whitespace():
 def test_punct_whitespace_preserved():
     html = (u'<div><span>по</span><span>ле</span>, and  ,  '
             u'<span>more </span>!<span>now</div>a (<b>boo</b>)')
-    assert (extract_text(html, guess_punct_space=True) ==
-            u'по ле, and , more ! now a (boo)')
+    assert (extract_text(
+        html, guess_punct_space=True) == u'по ле, and , more ! now a (boo)')
+
 
 def test_selector(all_options):
     html = '<div><div id="extract-me">text<div>more</div></div>and more text</div>'
@@ -64,32 +70,36 @@ def test_selector(all_options):
 
 def test_html_to_text():
     html = (u'<title>  title  </title><div>text_1.<p>text_2 text_3</p><ul>'
-             '<li>text_4</li><li>text_5</li></ul><p>text_6<em>text_7</em>'
-             'text_8</p>text_9</div><p>...text_10</p>')
+            '<li>text_4</li><li>text_5</li></ul><p>text_6<em>text_7</em>'
+            'text_8</p>text_9</div><p>...text_10</p>')
 
     parser = lxml.html.HTMLParser(encoding='utf8')
     tree = lxml.html.fromstring(html.encode('utf8'), parser=parser)
 
-    assert (html_to_text(tree, guess_punct_space=False) ==
-                                    ('title text_1. text_2 text_3 text_4 text_5'
-                                    ' text_6 text_7 text_8 text_9 ...text_10'))
-    assert (html_to_text(tree, guess_punct_space=False, guess_page_layout=True) ==
-                    ('title\n\n text_1.\n\n text_2 text_3\n\n text_4\n text_5'
-                    '\n\n text_6 text_7 text_8\n\n text_9\n\n ...text_10'))
-    assert (html_to_text(tree, guess_punct_space=True) ==
-                                ('title text_1. text_2 text_3 text_4 text_5'
-                                ' text_6 text_7 text_8 text_9...text_10'))
-    assert (html_to_text(tree, guess_punct_space=True, guess_page_layout=True) ==
-                          ('title\n\ntext_1.\n\ntext_2 text_3\n\ntext_4\ntext_5'
-                          '\n\ntext_6 text_7 text_8\n\ntext_9\n\n...text_10'))
+    assert (html_to_text(tree, guess_punct_space=False) == (
+        'title text_1. text_2 text_3 text_4 text_5'
+        ' text_6 text_7 text_8 text_9 ...text_10'))
+    assert (html_to_text(
+        tree, guess_punct_space=False, guess_page_layout=True) == (
+            'title\n\n text_1.\n\n text_2 text_3\n\n text_4\n text_5'
+            '\n\n text_6 text_7 text_8\n\n text_9\n\n ...text_10'))
+    assert (html_to_text(
+        tree,
+        guess_punct_space=True) == ('title text_1. text_2 text_3 text_4 text_5'
+                                    ' text_6 text_7 text_8 text_9...text_10'))
+    assert (html_to_text(
+        tree, guess_punct_space=True, guess_page_layout=True) == (
+            'title\n\ntext_1.\n\ntext_2 text_3\n\ntext_4\ntext_5'
+            '\n\ntext_6 text_7 text_8\n\ntext_9\n\n...text_10'))
+
 
 def test_guess_page_layout():
     html = (u'<title>  title  </title><div>text_1.<p>text_2 text_3</p>'
-             '<p id="demo"></p><ul><li>text_4</li><li>text_5</li></ul>'
-             '<p>text_6<em>text_7</em>text_8</p>text_9</div>'
-             '<script>document.getElementById("demo").innerHTML = '
-             '"This should be skipped";</script> <p>...text_10</p>'
-           )
-    assert (extract_text(html, guess_punct_space=True, guess_page_layout=True) ==
-                          ('title\n\ntext_1.\n\ntext_2 text_3\n\ntext_4\ntext_5'
-                          '\n\ntext_6 text_7 text_8\n\ntext_9\n\n...text_10'))
+            '<p id="demo"></p><ul><li>text_4</li><li>text_5</li></ul>'
+            '<p>text_6<em>text_7</em>text_8</p>text_9</div>'
+            '<script>document.getElementById("demo").innerHTML = '
+            '"This should be skipped";</script> <p>...text_10</p>')
+    assert (extract_text(
+        html, guess_punct_space=True, guess_page_layout=True) == (
+            'title\n\ntext_1.\n\ntext_2 text_3\n\ntext_4\ntext_5'
+            '\n\ntext_6 text_7 text_8\n\ntext_9\n\n...text_10'))
