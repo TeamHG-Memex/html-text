b8e5f55cc75abcf6f2d95a3a145afba87fdc28e9
Mikhail Korobov <kmike84@gmail.com>
2018-09-25 23:49:43 +0500

    Bump version: 0.4.0 → 0.4.1

--- html_text/__init__.py
+++ html_text/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 from .html_text import (extract_text, parse_html, cleaned_selector,
                         selector_to_text, NEWLINE_TAGS, DOUBLE_NEWLINE_TAGS)
