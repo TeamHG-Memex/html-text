ce3aa606b93b4d07bbe8fecba283a107a4c881be
Mikhail Korobov <kmike84@gmail.com>
2020-07-23 00:12:07 +0500

    Bump version: 0.5.1 → 0.5.2

--- html_text/__init__.py
+++ html_text/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 
 from .html_text import (etree_to_text, extract_text, selector_to_text,
                         parse_html, cleaned_selector, cleaner,
